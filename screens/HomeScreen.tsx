00d9e9527ab3a5bf5bdd795efdb8d2a42bd900e9
Shivam patel <yodashiv@gmail.com>
2021-01-27 15:48:18 -0800

    Delete all unneeded items on home

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -10,10 +10,10 @@ export default function HomeScreen() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Home Screen</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      {/* <Text style={styles.title}>Home Screen</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" /> */}
       {/* <EditScreenInfo path="/screens/HomeScreen.tsx" /> */}
-      <RestaurantSearch/>
+      {/* <RestaurantSearch/> */}
       <MapEmbed/>
     </View>
   );
