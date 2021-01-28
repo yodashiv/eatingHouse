a42b10a2efdb546bb647e877a6e4b995f9d8290a
Shivam patel <yodashiv@gmail.com>
2021-01-27 16:07:19 -0800

    onFocus for home search navigates to tabtwo

--- components/SearchBarPopout.tsx
+++ components/SearchBarPopout.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import { SearchBar } from 'react-native-elements';
+import { useNavigation } from '@react-navigation/native';
 
 export default function SearchBarPopout() {
+    const navigation = useNavigation();
+    const self = useRef(null);
+
     return (
         <SearchBar
         placeholder="Search"
-        onFocus={() => console.log("The search bar was pressed")}
+        ref={self}
+        onFocus={() => {
+            console.log("The search bar was pressed");
+            navigation.navigate("TabTwo");
+            self.current.blur();
+        }}
       />
     );
 }
